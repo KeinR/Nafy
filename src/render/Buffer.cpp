991794f9ca924fae30b503db721b1efbe3d9b1c3
Kein <orionmusselman@gmail.com>
2020-06-11 14:23:19 -0700

    Rolled back changes to Buffer
    I didn't understand vertex objects and their relation to buffers correctly - I thought they were just render configurations, but it turns out they become entwined with the buffers...

--- src/render/Buffer.cpp
+++ src/render/Buffer.cpp
@@ -5,44 +5,49 @@
 
 #include "../core/glfw.h"
 
-nafy::Buffer::Buffer() {
+nafy::Buffer::Buffer(initfunc_t initFunc) {
+    setInit(initFunc);
+    gen();
     init();
 }
 
-nafy::Buffer::Buffer(unsigned int vertices, unsigned int indices, unsigned int countIndices):
-    vertices(vertices), indices(indices), countIndices(countIndices) {
-}
-
 nafy::Buffer::~Buffer() {
-    deInit();
+    deGen();
 }
 
 nafy::Buffer::Buffer(Buffer &&other) {
     steal(other);
 }
 nafy::Buffer::Buffer(const Buffer &other) {
+    glGenBuffers(1, &vertices);
+    glGenBuffers(1, &indices);
     copy(other);
 }
 nafy::Buffer &nafy::Buffer::operator=(Buffer &&other) {
-    deInit();
+    deGen();
     steal(other);
     return *this;
 }
 nafy::Buffer &nafy::Buffer::operator=(const Buffer &other) {
+    glDeleteVertexArrays(1, &array);
     copy(other);
     return *this;
 }
 
 void nafy::Buffer::steal(Buffer &other) {
+    array = other.array;
     vertices = other.vertices;
     indices = other.indices;
     countIndices = other.countIndices;
+    other.array = 0;
     other.vertices = 0;
     other.indices = 0;
     other.countIndices = 0;
 }
 
 void nafy::Buffer::copy(const Buffer &other) {
+    glGenVertexArrays(1, &array);
+    std::cout << "copy" << std::endl;
     // Vertex buffer
     int vertSize;
     other.bindVert();
@@ -72,20 +77,41 @@ void nafy::Buffer::copy(const Buffer &other) {
     glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_COPY_WRITE_BUFFER, 0, 0, elemSize);
 
     countIndices = other.countIndices;
+    initFunc = other.initFunc;
+
+    init();
 }
 
-void nafy::Buffer::init() {
+void nafy::Buffer::gen() {
+    glGenVertexArrays(1, &array);
     glGenBuffers(1, &vertices);
     glGenBuffers(1, &indices);
     countIndices = 0;
 }
 
-void nafy::Buffer::deInit() {
-    glDeleteBuffers(1, &vertices);
-    glDeleteBuffers(1, &indices);
+void nafy::Buffer::deGen() {
+    if (array) {
+        glDeleteVertexArrays(1, &array);
+        glDeleteBuffers(1, &vertices);
+        glDeleteBuffers(1, &indices);
+    }
     countIndices = 0;
 }
 
+void nafy::Buffer::setInit(initfunc_t initFunc) {
+    this->initFunc = initFunc;
+}
+
+void nafy::Buffer::init() {
+    bind();
+    if (initFunc != nullptr) {
+        initFunc(*this);
+    }
+}
+
+void nafy::Buffer::bindArray() const {
+    glBindVertexArray(array);
+}
 void nafy::Buffer::bindVert() const {
     glBindBuffer(GL_ARRAY_BUFFER, vertices);
 }
@@ -93,6 +119,7 @@ void nafy::Buffer::bindElem() const {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices);
 }
 void nafy::Buffer::bind() const {
+    bindArray();
     bindVert();
     bindElem();
 }
@@ -105,6 +132,11 @@ void nafy::Buffer::setIndices(int count, unsigned int *data) {
     countIndices = count;
     bindElem();
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int), data, GL_STATIC_DRAW);
+}
+void nafy::Buffer::setParam(unsigned int index, int size, std::size_t stride, const void *offset) {
+    bind();
+    glVertexAttribPointer(index, size, GL_FLOAT, GL_FALSE, stride, offset);
+    glEnableVertexAttribArray(index);
 }
 
 void nafy::Buffer::render() {
--- src/render/Buffer.h
+++ src/render/Buffer.h
@@ -3,34 +3,48 @@
 
 #include "../gui/renderable.h"
 
+#include <cstddef>
+
 // Holds the raw vertex data
 
 namespace nafy {
     class Buffer: public renderable {
+    public:
+        typedef void(*initfunc_t)(Buffer &buffer);
+    private:
+        unsigned int array;
         unsigned int vertices;
         unsigned int indices;
 
         int countIndices;
 
+        // So that we can copy this
+        initfunc_t initFunc;
+
         void copy(const Buffer &other);
         void steal(Buffer &other);
-        void init();
-        void deInit();
+        void gen();
+        void deGen();
     public:
-        Buffer();
-        Buffer(unsigned int vertices, unsigned int indices, unsigned int countIndices);
+        // NB: If copied while init is nullptr, the copied will be ill-formed!
+        Buffer(initfunc_t init = nullptr);
         ~Buffer();
         Buffer(Buffer &&other);
         Buffer(const Buffer &other);
         Buffer &operator=(Buffer &&other);
         Buffer &operator=(const Buffer &other);
 
+        void setInit(initfunc_t initFunc);
+        void init();
+
+        void bindArray() const;
         void bindVert() const;
         void bindElem() const;
         void bind() const;
 
         void setVerticies(int count, float *data);
         void setIndices(int count, unsigned int *data);
+        void setParam(unsigned int index, int size, std::size_t stride, const void *offset);
 
         void render() override;
     };
